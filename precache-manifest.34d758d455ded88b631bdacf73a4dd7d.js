ac1c7e3a0913b052551b09e18be31b6d94582d18
noormushtaq <noormushtaq1985@gmail.com>
2019-12-25 18:02:03 -0600

    Updates

---
+++ precache-manifest.34d758d455ded88b631bdacf73a4dd7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca12aaca137ae21ed7f968fefd94a233",
+    "url": "/bmi-Calculator/index.html"
+  },
+  {
+    "revision": "39a89140c5b06e9c526c",
+    "url": "/bmi-Calculator/static/css/main.d0edd1a0.chunk.css"
+  },
+  {
+    "revision": "b2d37c086625fb0b2c2d",
+    "url": "/bmi-Calculator/static/js/2.43f32747.chunk.js"
+  },
+  {
+    "revision": "39a89140c5b06e9c526c",
+    "url": "/bmi-Calculator/static/js/main.8e012f0d.chunk.js"
+  },
+  {
+    "revision": "8ac5342132c20cbecf24",
+    "url": "/bmi-Calculator/static/js/runtime-main.78a9e182.js"
+  }
+]);
